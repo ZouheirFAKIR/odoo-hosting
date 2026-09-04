1fb201002268cffb31119c7530b7b905f112cca4
zouheir-python <fakirzouheir@gmail.com>
2026-09-04 14:30:01 +0100

    switch to normal startup now that database is fully initialized

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo "===== START.SH IS RUNNING ====="
-exec odoo --db_host="$HOST" --db_port=5432 --db_user="$USER" --db_password="$PASSWORD" --db_sslmode=require -d neondb -i base --without-demo=all
+exec odoo --db_host="$HOST" --db_port=5432 --db_user="$USER" --db_password="$PASSWORD" --db_sslmode=require -d neondb --without-demo=all
